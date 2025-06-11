1a413304edc825797697b358e5f3c8ad12a3cd02
Предраг Николић <idmpepe@gmail.com>
2025-06-11 19:42:26 +0200

    changes

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from Mir import LanguageServer, electron_node_20, PackageStorage
 
 
-server_storage = PackageStorage(tag='0.0.1', sync_folder="./language-server")
+server_storage = PackageStorage(tag='0.0.1')
+server_path = server_storage / "language-server" / "out" / 'eslintServer.js'
 
+async def package_storage_setup():
+    if server_path.exists():
+        return
+    await electron_node_20.setup()
+    server_storage.copy("./language-server")
+    with LoaderInStatusBar(f'installing eslint'):
+        await command([deno.path, "install"], cwd=str(server_storage / "language-server"))
 
 class EslintLanguageServer(LanguageServer):
     name='eslint'
@@ -12,8 +20,9 @@ class EslintLanguageServer(LanguageServer):
     settings_file="Mir-eslint.sublime-settings"
 
     async def activate(self):
-        await electron_node_20.setup()
-        server_path = server_storage / "language-server" / "out" / 'eslintServer.js'
+        # setup runtime and install dependencies
+        await package_storage_setup()
+
         self.settings.set('workspaceFolder', self.initialize_params.get('workspaceFolders', [])[0])
 
         def handle_status(_):
